8973bdee57e6217621d226456c15739554aa7007
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-20 20:11:56 -0300

    Validando área logada

--- nflix/features/step_definitions/login_steps.rb
+++ nflix/features/step_definitions/login_steps.rb
@@ -1,18 +1,19 @@
-Quando('eu faço login com {string} e {string}') do |email, senha|
+Quando("eu faço login com {string} e {string}") do |email, senha|
     visit "/"
     find("#emailId").set email
     find("#passId").set senha
     click_button "Entrar"
   end
   
-  Então('devo ser autenticado') do
+  Então("devo ser autenticado") do
     js_script = 'return window.localStorage.getItem("default_auth_token");'
     token = page.execute_script(js_script)
     # log(token)
     expect(token.length).to be 147
   end
   
-  Então('devo ver {string} na área logada') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+  Então("devo ver {string} na área logada") do |expect_name|
+    user = find(".sidebar-wrapper .user .info span")
+    expect(user.text).to eql expect_name
   end
   
