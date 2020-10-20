5bc2d57017726a60010838254ea5b4024b13a149
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-20 20:25:23 -0300

    Validações com CSS Selector

--- nflix/features/step_definitions/login_steps.rb
+++ nflix/features/step_definitions/login_steps.rb
@@ -16,4 +16,14 @@ Quando("eu faço login com {string} e {string}") do |email, senha|
     user = find(".sidebar-wrapper .user .info span")
     expect(user.text).to eql expect_name
   end
+
+  Então('não devo ser autenticado') do
+    js_script = 'return window.localStorage.getItem("default_auth_token");'
+    token = page.execute_script(js_script)
+    expect(token).to be nil
+  end
+  
+  Então('devo ver a mensagem de alerta {string}') do |expect_message|
+    expect(find(".alert").text).to eql expect_message
+  end
   
