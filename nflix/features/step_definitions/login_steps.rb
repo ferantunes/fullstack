09ebe1b5bdd72ccdfcf05379604c469388d87390
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-20 22:01:04 -0300

    Support Code

--- nflix/features/step_definitions/login_steps.rb
+++ nflix/features/step_definitions/login_steps.rb
@@ -5,10 +5,8 @@ Quando("eu faço login com {string} e {string}") do |email, senha|
   end
   
   Então("devo ser autenticado") do
-    js_script = 'return window.localStorage.getItem("default_auth_token");'
-    token = page.execute_script(js_script)
     # log(token)
-    expect(token.length).to be 147
+    expect(get_token.length).to be 147
   end
   
   Então("devo ver {string} na área logada") do |expect_name|
@@ -16,9 +14,7 @@ Quando("eu faço login com {string} e {string}") do |email, senha|
   end
 
   Então('não devo ser autenticado') do
-    js_script = 'return window.localStorage.getItem("default_auth_token");'
-    token = page.execute_script(js_script)
-    expect(token).to be nil
+    expect(get_token).to be nil
   end
   
   Então('devo ver a mensagem de alerta {string}') do |expect_message|
--- nflix/features/support/env.rb
+++ nflix/features/support/env.rb
@@ -2,6 +2,11 @@ require "capybara"
 require "capybara/cucumber"
 require "selenium-webdriver"
 
+require_relative "helpers"
+
+# método nativo que pode ser invocado a qqr momento e em qqr lugar
+World(Helpers)
+
 Capybara.configure do |config|
     config.default_driver = :selenium
     config.app_host = "http://127.0.0.1:8080"
